24071396daec4b566ea6aa7a0094bbcd2ca6da55
neharepos <nehakeshrimail@gmail.com>
2024-04-25 21:33:26 +0530

    added feature to store authentication detail as cookies

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const {v4:uuidv4} = require('uuid')
 const User = require("../models/user");
-const {setUser} = require('../service/auth')
+const {setUser} = require('../service/auth');
 
 async function handleUserSignup(req, res) {
     const { name, email, password } = req.body;
@@ -14,15 +14,14 @@ async function handleUserSignup(req, res) {
 
 async function handleUserLogin(req, res) {
     const { email, password } = req.body;
-    const user = await User.findOne({email,password});
+    const user = await User.findOne({email, password});
     if (!user)
         return res.render("login", {
             error: "Invalid Username or Password",
 });
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie("uid", sessionId);
-    return res.redirect("/");
+    const token = setUser(user);
+    // res.cookie("uid", token);   
+    return res.json( { token });
 }
 
 module.exports = {

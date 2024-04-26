2e32424c2ed1351c88adde4e7714ce956d1eb3fc
neharepos <nehakeshrimail@gmail.com>
2024-04-26 10:54:39 +0530

    added authorization feature

--- controllers/user.js
+++ controllers/user.js
@@ -20,8 +20,8 @@ async function handleUserLogin(req, res) {
             error: "Invalid Username or Password",
 });
     const token = setUser(user);
-    // res.cookie("uid", token);   
-    return res.json( { token });
+    res.cookie("token", token);   
+    return res.redirect("/");
 }
 
 module.exports = {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,32 @@
 const { getUser } = require("../service/auth");
 
-async function restrictToLoggedinUserOnly(req, res, next) {
-    const userUid = req.headers["Authorization"];
 
-    if (!userUid) return res.redirect("/login");
-    const token = userUid.split("Bearer")[1];
-    const user = getUser(token);
+function checkForAuthentication(req, res, next){
+    const tokenCookie = req.cookies?.token;
+    req.user = null;
 
-    if (!user) return res.redirect("/login");
+    if(!tokenCookie) return next();
 
-    req.user = user;
-    next();
-}
-
-async function checkAuth(req, res, next) {
-    const userUid = req.headers["authorization"];
-    const token = userUid.split("Bearer")[1];
+    const token = tokenCookie;
     const user = getUser(token);
 
     req.user = user;
-    next();
+    return next();
 }
+
+
+function restrictTo(roles = []) {
+    return function(req, res, next){
+        if(!req.user) return res.redirect("/login");
+
+        if(!roles.includes(req.user.role)) return res.end("UnAuthorized");
+
+        return next();
+    };
+}
+
 
 module.exports = {
-    restrictToLoggedinUserOnly,
-    checkAuth,
+    checkForAuthentication,
+    restrictTo,
 };
